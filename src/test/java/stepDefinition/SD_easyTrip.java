4797b36bdb12f1d36ca76b774b5b0c018eba770c
tamojit02 <=>
2025-01-19 00:12:52 +0530

    test commit

--- src/test/java/stepDefinition/SD_easyTrip.java
+++ src/test/java/stepDefinition/SD_easyTrip.java
@@ -3,18 +3,28 @@ package stepDefinition;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SD_easyTrip {
 
+    WebDriver driver = new ChromeDriver();
+
     @Given("^I want to select travelType (.+)$")
-    public void i_want_to_select_travel_type(String travelType) {
+    public void i_want_to_select_travel_type(String travelType) throws InterruptedException {
         System.out.println(travelType);
+        driver.get("https://www.easemytrip.com/");
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+
+
 
     }
 
     @When("^I want to select formcity as (.+)$")
     public void i_want_to_selectFormCity(String formCity) {
         System.out.println(formCity);
+        driver.quit();
         // Write code here that turns the phrase above into concrete actions
 
     }
